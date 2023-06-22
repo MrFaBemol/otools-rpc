8f74507e6109a7d9098b0d0c8e646d40fdfb0c9a
MrFaBemol <mrfabemol@octoopus.org>
2023-06-22 10:38:23 +0200

    [VERSION] 0.4.0
    Add a cache system to the lib

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="otools-rpc",
-    version="0.3.1",
+    version="0.4.0",
     description="A tool to interact with Odoo's external API.",
     packages=find_packages(exclude=["tests"]),
     long_description=long_description,
