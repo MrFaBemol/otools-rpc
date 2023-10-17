13d3ba305b0af249852d6033951f2208f11ff8d0
MrFaBemol <mrfabemol@octoopus.org>
2023-10-17 10:51:33 +0200

    [VERSION] 0.4.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="otools-rpc",
-    version="0.4.2",
+    version="0.4.3",
     description="A tool to interact with Odoo's external API.",
     packages=find_packages(exclude=["tests"]),
     long_description=long_description,
